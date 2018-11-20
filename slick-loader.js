c672042a40da3d9ee73ea2fd7643c8b315c0f536
Zenoo <fzenooo@gmail.com>
2018-11-20 10:55:14 +0100

    Allow access to SlickLoader from the very beginning

--- slick-loader.js
+++ slick-loader.js
@@ -57,34 +57,57 @@ class Slickloader{
 	 * @param {(Element|String)} parent The wrapper for the SlickLoader module
 	 */
 	constructor(parent){
-		/**
-		 * Loader parent element
-		 * @private
-		 */
-		this._parent = parent ? parent instanceof Element ? parent : document.querySelector(parent) : document.getElementsByTagName('body')[0];
+		this._waitForBody().then(() => {
+			/**
+			 * Loader parent element
+			 * @private
+			 */
+			this._parent = parent ? parent instanceof Element ? parent : document.querySelector(parent) : document.body;
 
-		//Errors checking
-		if(!this._parent) throw new Error('SlickLoader: '+(typeof parent == 'string' ? 'The selector `'+parent+'` didn\'t match any element.' : 'The element you provided was undefined'));
-		if(Array.from(this._parent.children).some(e => e.matches('.slick-loader'))) console.warn('SlickLoader: The loader has already been initialized in this context.');
+			//Errors checking
+			if(!this._parent) throw new Error('SlickLoader: '+(typeof parent == 'string' ? 'The selector `'+parent+'` didn\'t match any element.' : 'The element you provided was undefined'));
+			if(Array.from(this._parent.children).some(e => e.matches('.slick-loader'))) console.warn('SlickLoader: The loader has already been initialized in this context.');
 
-		/**
-		 * The loader element
-		 * @type {Element}
-		 */
-		this.element = document.createElement('div');
-		this.element.classList.add('slick-loader');
-		this.element.innerHTML = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-eclipse" style="background: none;"> <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#ffffff" transform="rotate(172.988 50 51)"> <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform> </path> <text style="cursor: move;" stroke="#000" xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_4" y="48.875258" x="50" stroke-opacity="null" stroke-width="0" fill="#ffffff"></text> <text xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_5" y="55.010217" x="50" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#ffffff"></text> </svg>';
-		 
-		if(parent) this.element.classList.add('slick-loader-inner');
+			/**
+			 * The loader element
+			 * @type {Element}
+			 */
+			this.element = document.createElement('div');
+			this.element.classList.add('slick-loader');
+			this.element.innerHTML = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-eclipse" style="background: none;"> <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#ffffff" transform="rotate(172.988 50 51)"> <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 51;360 50 51" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform> </path> <text style="cursor: move;" stroke="#000" xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_4" y="48.875258" x="50" stroke-opacity="null" stroke-width="0" fill="#ffffff"></text> <text xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_5" y="55.010217" x="50" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#ffffff"></text> </svg>';
+			
+			if(parent) this.element.classList.add('slick-loader-inner');
 
-		this._parent.appendChild(this.element);
+			this._parent.appendChild(this.element);
+		});
+	}
+
+	/**
+	 * Wait for the body tag to be loaded
+	 * @private
+	 */
+	_waitForBody(){
+		return new Promise(resolve => {
+			if(document.body) resolve();
+			else{
+				let observer = new MutationObserver(function() {
+					if (document.body) {
+						observer.disconnect();
+						resolve();
+					}
+				});
+				observer.observe(document.documentElement, {childList: true});
+			}
+		});
 	}
 
 	/**
 	 * Displays the 
 	 */
 	enable(){
-		this.element.classList.add('active')
+		this._waitForBody().then(() => {
+			this.element.classList.add('active');
+		});
 
 		return this;
 	}
@@ -93,7 +116,9 @@ class Slickloader{
 	 * Hides the Slickloader
 	 */
 	disable(){
-		this.element.classList.remove('active');
+		this._waitForBody().then(() => {
+			this.element.classList.remove('active');
+		});
 
 		return this;
 	}
@@ -104,10 +129,12 @@ class Slickloader{
 	 * @param {String} text2 The second line of text
 	 */
 	setText(text1,text2){
-		const textList = this.element.getElementsByTagName('text');
+		this._waitForBody().then(() => {
+			const textList = this.element.getElementsByTagName('text');
 
-		if(text1 !== undefined) textList[0].innerHTML = text1;
-		if(text2 !== undefined) textList[1].innerHTML = text2;
+			if(text1 !== undefined) textList[0].innerHTML = text1;
+			if(text2 !== undefined) textList[1].innerHTML = text2;
+		});
 
 		return this;
 	}
@@ -116,9 +143,12 @@ class Slickloader{
 	 * Clears any text from the Slickloader
 	 */
 	clearText(){
-		let textList = this.element.getElementsByTagName('text');
-		textList[0].innerHTML = '';
-		textList[1].innerHTML = '';
+		this._waitForBody().then(() => {
+			const textList = this.element.getElementsByTagName('text');
+
+			textList[0].innerHTML = '';
+			textList[1].innerHTML = '';
+		});
 
 		return this;
 	}
@@ -127,7 +157,9 @@ class Slickloader{
 	 * Removes any Slickloader mutation from the DOM
 	 */
 	destroy(){
-		this.element.remove();
+		this._waitForBody().then(() => {
+			this.element.remove();
+		});
 	}
 
 	/**
@@ -146,12 +178,5 @@ class Slickloader{
 	}
 }
 
-let observer = new MutationObserver(function() {
-	if (document.body) {
-		// It exists now
-		window.SlickLoader = new Slickloader();
-		document.body.appendChild(LOADER);
-		observer.disconnect();
-	}
-});
-observer.observe(document.documentElement, {childList: true});
+// Init default SlickLoader
+window.SlickLoader = new Slickloader();
