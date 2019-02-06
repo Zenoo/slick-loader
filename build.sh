ad360ace67d4bc73b147ae6ed4bd7315070f7f00
Zenoo <fzenooo@gmail.com>
2019-02-06 10:23:49 +0100

    build.sh exit code

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-npx babel src -o slick-loader.min.js && jsdoc . -R README.md -c docs/jsdoc-conf.json && git add *
+npx babel src -o slick-loader.min.js && jsdoc . -R README.md -c docs/jsdoc-conf.json && git add * && git commit -m "Babel & JSDoc generation"
+echo $?
