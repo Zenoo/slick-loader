43f6c4e6aaa0a46a301016c491989c88e4128251
Zenoo <fzenooo@gmail.com>
2019-02-12 10:24:32 +0100

    Added back the deprecated LOADER variable

--- src/slick-loader.js
+++ src/slick-loader.js
@@ -7,18 +7,18 @@ if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.ms
  * @deprecated since v1.1.0. Disappears in v1.2.0
  * Use SlickLoader instead
  */
-const LOADER = document.createElement('div');
-LOADER.classList.add('loadingScreen');
-LOADER.innerHTML = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-eclipse" style="background: none;"> <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#ffffff"></path> <text style="cursor: move;" stroke="#000" xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_4" y="48.875258" x="50" stroke-opacity="null" stroke-width="0" fill="#ffffff"></text> <text xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_5" y="55.010217" x="50" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#ffffff"></text> </svg>';
+window.LOADER = document.createElement('div');
+window.LOADER.classList.add('loadingScreen');
+window.LOADER.innerHTML = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-eclipse" style="background: none;"> <path ng-attr-d="{{config.pathCmd}}" ng-attr-fill="{{config.color}}" stroke="none" d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill="#ffffff"></path> <text style="cursor: move;" stroke="#000" xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_4" y="48.875258" x="50" stroke-opacity="null" stroke-width="0" fill="#ffffff"></text> <text xml:space="preserve" text-anchor="middle" font-family="Helvetica, Arial, sans-serif" font-size="5" id="svg_5" y="55.010217" x="50" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#ffffff"></text> </svg>';
 
 if(document.body){
-	document.body.appendChild(LOADER);
+	document.body.appendChild(window.LOADER);
 }
 else{
 	let observer = new MutationObserver(function() {
 		if (document.body) {
 			observer.disconnect();
-			document.body.appendChild(LOADER);
+			document.body.appendChild(window.LOADER);
 		}
 	});
 	observer.observe(document.documentElement, {childList: true});
@@ -28,44 +28,44 @@ else{
  * @deprecated since v1.1.0. Disappears in v1.2.0
  * Use SlickLoader.enable() instead
  */
-LOADER.enable = () => {
+window.LOADER.enable = () => {
 	console.warn('LOADER is depcrecated. Use SlickLoader instead.');
-    LOADER.classList.add('active');
-    return LOADER;
+    window.LOADER.classList.add('active');
+    return window.LOADER;
 };
 
 /**
  * @deprecated since v1.1.0. Disappears in v1.2.0
  * Use SlickLoader.disable() instead
  */
-LOADER.disable = () => {
+window.LOADER.disable = () => {
 	console.warn('LOADER is depcrecated. Use SlickLoader instead.');
-    LOADER.classList.remove('active');
-    return LOADER;
+    window.LOADER.classList.remove('active');
+    return window.LOADER;
 };
 
 /**
  * @deprecated since v1.1.0. Disappears in v1.2.0
  * Use SlickLoader.SetText() instead
  */
-LOADER.setText = (text1,text2) => {
+window.LOADER.setText = (text1,text2) => {
 	console.warn('LOADER is depcrecated. Use SlickLoader instead.');
-    let textList = LOADER.getElementsByTagName('text');
+    let textList = window.LOADER.getElementsByTagName('text');
     if(text1 !== undefined) textList[0].innerHTML = text1;
     if(text2 !== undefined) textList[1].innerHTML = text2;
-    return LOADER;
+    return window.LOADER;
 };
 
 /**
  * @deprecated since v1.1.0. Disappears in v1.2.0
  * Use SlickLoader.clearText() instead
  */
-LOADER.clearText = () => {
+window.LOADER.clearText = () => {
 	console.warn('LOADER is depcrecated. Use SlickLoader instead.');
-    let textList = LOADER.getElementsByTagName('text');
+    let textList = window.LOADER.getElementsByTagName('text');
     textList[0].innerHTML = '';
     textList[1].innerHTML = '';
-    return LOADER;
+    return window.LOADER;
 };
 
 /** Slickloader class used for the SlickLoader element */
